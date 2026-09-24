ac9213f6f2eb7e4bdcef21f42447360ea872637a
beyonder96 <kennedoliveiratm@gmail.com>
2026-09-24 05:54:44 -0300

    chore(release): bump version to v1.7.0 (code 17)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tessera.browser"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "1.6.0"
+        versionCode = 17
+        versionName = "1.7.0"
     }
 
     buildTypes {
