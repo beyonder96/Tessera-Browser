b14aa87ee0056fc6a81ab0019658da2ba2de00b7
beyonder96 <kennedoliveiratm@gmail.com>
2026-09-25 12:34:30 -0300

    chore(release): bump version to v2.3.0 (code 23)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tessera.browser"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "2.2.0"
+        versionCode = 23
+        versionName = "2.3.0"
     }
 
     buildTypes {
