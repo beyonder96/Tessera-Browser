f58fe2cee6ad0fcbacaa1094d4184a08f9f5ff23
beyonder96 <kennedoliveiratm@gmail.com>
2026-09-26 13:34:23 -0300

    chore(release): v2.5.3 (versionCode 29)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tessera.browser"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "2.5.2"
+        versionCode = 29
+        versionName = "2.5.3"
     }
 
     buildTypes {
